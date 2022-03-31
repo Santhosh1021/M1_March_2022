25690f670e7f2de76229bed128f26ef1f5973103
Santhosh1021 <101259618+Santhosh1021@users.noreply.github.com>
2022-03-31 12:55:42 +0530

    Create config.h

---
+++ 3_Implementation/inc/config.h
@@ -0,0 +1,28 @@
+/**
+ * @file project_config.h
+ * @author Santhosh
+ * @version 0.1
+ * @date 2022-03-31
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __PROJECT_CONFIG_H__
+#define __PROJECT_CONFIG_H__
+/**
+ * Include files
+ */ 
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * Function declarations
+ */
+void addrecord();
+void deleterecord();
+void displayrecord();
+void modifyrecord();
+void gotoxy(int x, int y);
+
+#endif /* __PROJECT_CONFIG_H__ */
