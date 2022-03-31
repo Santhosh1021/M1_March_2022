c2d2fafeebf1e588eb41e670c40dab04ccc9ece2
Santhosh1021 <101259618+Santhosh1021@users.noreply.github.com>
2022-03-31 12:07:00 +0530

    Create Version1.h
    Added version1.h

---
+++ 3_Implementation/inc/Version1.h
@@ -0,0 +1,31 @@
+/**
+ * @file  version1.h
+ * @author Santhosh 
+ * @version 0.1
+ * @date 2022-03-31
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __VERSION1_H__
+#define __VERSION1_H__
+
+/**
+ * Include files
+ */ 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+/**
+ * Function declarations
+ */
+void addrecord();
+void deleterecord();
+void displayrecord();
+void modifyrecord();
+void gotoxy(int x, int y);
+
+#endif /** __VERSION1_H__ */
