fdc4e42eaeaf64326c5d4563a97fdc968710f313
Santhosh1021 <101259618+Santhosh1021@users.noreply.github.com>
2022-04-03 08:52:40 +0530

    Create employee_management.h

---
+++ 3_Implementation/inc/employee_management.h
@@ -0,0 +1,22 @@
+/**
+ * @file employee_management.h
+ * @author Santhosh A
+ * @brief Header file for employee management with all functions
+ *
+ */
+
+#ifndef __EMPLOYEE_MANAGEMENT_H__
+#define __EMPLOYEE_MANAGEMENT_H__
+
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+void gotoxy(int x, int y);
+void S_Add_Record();
+void S_Delete_Record();
+void S_Display_Record();
+void S_Modify_Record();
+#endif  /* #define__EMPLOYEE_MANAGEMENT_H__ */
